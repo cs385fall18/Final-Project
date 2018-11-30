21f2a7a97ba19694dd89955ca18f2c60b40896d9
David Tauraso <david318@me.com>
2018-11-30 14:47:05 -0800

    Update gcloud_vm_setup.sh

--- gcloud_vm_setup.sh
+++ gcloud_vm_setup.sh
@@ -4,7 +4,7 @@ gcloud compute instances create lab06 --zone=us-west1-c --machine-type=n1-standa
 gcloud compute instances create finalproject --zone=us-east4-c --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server --image=ubuntu-1804-bionic-v20181029 --image-project=ubuntu-os-cloud --boot-disk-size=20GB --boot-disk-type=pd-standard --boot-disk-device-name=finalproject
 
 
-git clone -b david-register "https://github.com/cs385fall18/Final-Project.git"
+git clone -b david-token "https://github.com/cs385fall18/Final-Project.git"
 
 
 gcloud compute instances create lab07 --zone=us-west1-c --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server --image=ubuntu-1804-bionic-v20180823 --image-project=ubuntu-os-cloud --boot-disk-size=20GB --boot-disk-type=pd-standard --boot-disk-device-name=lab07
