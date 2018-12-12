972d720ced2095e12b9a805afab2ae33adb0c299
jcriseno <criseno@sonoma.edu>
2018-12-11 16:35:08 -0800

    Add files via upload

---
+++ NewProj/instapic/instapic/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.CharField(max_length=140)
+    password = models.CharField(max_length=100)
+    last_login = models.DateTimeField(auto_now=True)
